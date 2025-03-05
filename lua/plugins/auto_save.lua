d1f3b35a048662e7fcc9ae5ab13f37a522600076
Turann_ <turanull000@gmail.com>
2025-03-05 12:06:28 +0300

    Update auto_save.lua

--- lua/plugins/auto_save.lua
+++ lua/plugins/auto_save.lua
@@ -1,29 +1,25 @@
 return {
-    "Pocco81/auto-save.nvim",
-    config = function()
-        require("auto-save").setup({
-            enabled = true,
-            trigger_events = {"TextChanged", "FocusLost"},
-            execution_message = {
-            message = function()
-		return ("Saved at " .. vim.fn.strftime("%H:%M:%S"))
-		end,
-            condition = function(buf)
-                local fn = vim.fn
-                local utils = require("auto-save.utils.data")
-		local mode = vim.api.nvim_get_mode().mode
-					if mode == "c" then -- CLI Mode the mode you enter when pressed ESC 
-					return false
-				end
-                if
-                    fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
-                    return true -- met condition(s), can save
-                end
-                return false -- can't save
-            end,
-            clean_delay = 0,
-            },
-            debounce_delay = 500, -- Time in milliseconds
-        })
+  "Pocco81/auto-save.nvim",
+  config = function()
+    require("auto-save").setup({
+      enabled = true,
+      trigger_events = {"TextChanged", "TextChangedI", "CursorHoldI"},
+      execution_message = {
+        message = function()
+          local save_msg = string.format(("💾 %s at %s"):format(vim.fn.expand("%:t"), vim.fn.strftime("%H:%M")))
+          return vim.notify(save_msg)
+        end,
+      },
+      condition = function(buf)
+        local fn = vim.fn
+        local utils = require("auto-save.utils.data")
+        if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
+          return true -- Save (If file is modifiable and not blacklisted.)
         end
+        return false  -- Default to not save
+      end,
+      write_all_buffers = false, -- write all buffers when the current one meets `condition`
+      debounce_delay = 5000, -- saves the file at most every `debounce_delay` milliseconds
+    })
+  end
 }
