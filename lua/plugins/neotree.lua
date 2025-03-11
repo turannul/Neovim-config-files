be4cf1b3f97f133bb59fa7000f58c7bc5360c9d3
Turann_ <turanull000@gmail.com>
2025-03-11 23:54:13 +0300

    Update neotree.lua

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,44 +1,37 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    event = { "VeryLazy" },
-    position = "right",
-    keys = {
-      {
-        "<leader>e",
-        "<Cmd> Neotree toggle reveal_force_cwd <CR>",
-        desc = "File Explorer",
+  "nvim-neo-tree/neo-tree.nvim",
+  event = "VeryLazy",
+  keys = {
+    { "<leader>e", "<Cmd>Neotree toggle reveal_force_cwd<CR>", desc = "File Explorer" },
+    { "<leader>E", function() require("neo-tree.command").execute({ source = "git_status", toggle = true }) end, desc = "Git Explorer" },
+  },
+  dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("neo-tree").setup({
+      position = "right",  -- Ensure this is at root level
+      filesystem = {
+        filtered_items = { hide_dotfiles = false, hide_gitignored = false },
+        bind_to_cwd = true,
+        follow_current_file = true,
       },
-      {
-        "<leader>E",
-        function()
-          require("neo-tree.command").execute({ source = "git_status", toggle = true })
-        end,
-        desc = "Git Explorer",
+      window = {
+        position = "right",  -- Explicit window position
+        width = 35,
+        mappings = { ["<C-e>"] = "close_window" }
       },
-    },
-    deactivate = function()
-      vim.cmd([[Neotree close]])
-    end,
-    dependencies = {
-      { "MunifTanjim/nui.nvim" },
-      { "nvim-lua/plenary.nvim" },
-    },
-    init = function()
-      vim.api.nvim_create_autocmd("BufEnter", {
-        group = vim.api.nvim_create_augroup("NeOTree_Begin", { clear = true }),
-        once = true,
-        callback = function()
-          if package.loaded["neo-tree"] then
-            return
-          else
-            local stats = vim.uv.fs_stat(vim.fn.argv(0))
-            if stats and stats.type == "directory" then
-              require("neo-tree")
-            end
-          end
-        end,
-      })
-    end,
-  }, -- File Explorer
+      source_selector = { winbar = true, content_layout = "center" }
+    })
+  end,
+  init = function()
+    vim.api.nvim_create_autocmd("BufEnter", {
+      group = vim.api.nvim_create_augroup("NeoTreeInit", { clear = true }),
+      callback = function()
+        local stats = vim.loop.fs_stat(vim.fn.argv(0))
+        if stats and stats.type == "directory" then
+          require("neo-tree").close_all()
+          require("neo-tree").show({ position = "right", reveal_force_cwd = true })
+        end
+      end
+    })
+  end,
 }
