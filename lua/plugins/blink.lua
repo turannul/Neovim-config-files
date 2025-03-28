001045c54b96cb851466f15a683558941b1d68fa
Turann_ <turanull000@gmail.com>
2025-03-28 07:09:30 +0300

    new plugin, suggestions

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,17 @@
+return {
+    'saghen/blink.cmp',
+    dependencies = { 'rafamadriz/friendly-snippets' },
+    Event = 'InsertEnter',
+    version = '1.*',
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+        keymap = { preset = 'default' },
+        appearance = { nerd_font_variant = 'normal' },
+        completion = { documentation = { auto_show = false } },
+        sources = { default = { 'lsp', 'path', 'snippets', 'buffer' }, },
+        fuzzy = { implementation = "prefer_rust_with_warning" }
+    },
+    opts_extend = { "sources.default" }
+}
