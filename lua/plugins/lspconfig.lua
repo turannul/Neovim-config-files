32dbd3951add643c2a01e4e3c47276ecb3d0c581
Turann_ <turanull000@gmail.com>
2025-08-01 20:03:25 +0300

    Update lspconfig.lua

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,34 +1,14 @@
 return {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        },
-        config = function()
+    "neovim/nvim-lspconfig",
+    dependencies = {"williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim"},
+    config = function()
         require("mason").setup()
         require("mason-lspconfig").setup({
-            ensure_installed = {
-            "bashls",
-            "clangd",
-            "omnisharp",
-            "cssls",
-            "html",
-            "jsonls",
-            "lua_ls",
-            "perlnavigator",
-            "phpactor",
-            "pyright",
-            "rust_analyzer",
-            "tailwindcss",
-            "vimls",
-            "yamlls"
-            }
+            ensure_installed = {"autotools_ls", "cmake", "gh_actions_ls", "bashls", "clangd", "omnisharp", "cssls", "html", "jsonls", "lua_ls", "perlnavigator", "phpactor", "pyright", "rust_analyzer", "tailwindcss", "vimls", "yamlls"}
         })
-    
-        local lspconfig = require("lspconfig")
-    
+
         for _, server in ipairs(require("mason-lspconfig").get_installed_servers()) do
-            lspconfig[server].setup({})
+            require("lspconfig")[server].setup({})
         end
     end
 }
